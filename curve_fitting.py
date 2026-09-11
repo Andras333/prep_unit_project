33e747b1138b84c6cb3792eb634ee002a962c667
Andras333 <aszekely3@gmail.com>
2026-09-11 18:11:04 +0200

    Project is finished

--- curve_fitting.py
+++ curve_fitting.py
@@ -1,15 +1,18 @@
 from scipy.optimize import curve_fit
 
 def set_objective(x, a, b):
+    # print(x, a, b)
     """ return the objective function """
     #To-Do set the objective equation
-    pass
+    return a * x + b
 
 def get_result( x, y):
     """Return optimal values for a and b for the equation y = a*x+b """
 
     # curve fit
+    print('curve fitting')
     estimations, _ = curve_fit(set_objective, x, y)
+    print(estimations)
     # summarize the parameter values
     a, b = estimations
     print('y = %.5f * x + %.5f' % (a, b))
--- data1/database_manager.py
+++ data1/database_manager.py
@@ -2,16 +2,17 @@ import sqlite3
 from sqlite3 import Error
 
 
-def create_connection(db_file):
+def create_connection(connection_string):
+    print('checking')
     """ create a database connection to a SQLite database """
-    conn = None
+    conn = sqlite3.connect(connection_string)
     try:
-        conn = None  # To-Do add a connection for the database
+        conn = sqlite3.connect(connection_string)  # To-Do add a connection for the database
     except Error as e:
         print(e)
-
-    #To-Do return the connection 
-    return None 
+    print(conn)
+    #To-Do return the connection
+    return conn
 
 def close_connection(conn):
     """ closes a connection to a database """
@@ -21,13 +22,13 @@ def close_connection(conn):
 def select_all(conn):
     """select all rows from our table using the conn we already created """
     cur = conn.cursor()
-    query = "" # To-Do write the query to retrive all data from the longley table 
+    query = "SELECT * FROM longley"  # To-Do write the query to retrive all data from the longley table
 
     cur.execute(query)
 
-    rows = None  # To-Do fetch all rows using the cursor cur
+    rows = cur.fetchall()  # To-Do fetch all rows using the cursor cur
 
-    return rows 
+    return rows
 
 
 def print_rows(rows):
